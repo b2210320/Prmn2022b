408b89d2b91a4e898ffae498e4c380a9c04a6e5b
b2210320 <b2210320@photon.chitose.ac.jp>
2022-11-23 16:01:07 +0900

    課題1-2

---
+++ prmn2022b/src/lecture01/Kadai2.java
@@ -0,0 +1,12 @@
+package lecture01;
+
+import java.util.Scanner;
+
+public class Kadai2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("yourstudentnumber = ");
+        int studentNumber = input.nextInt();
+        System.out.println("B" + studentNumber + " Shogo Ihara");
+    }
+}
